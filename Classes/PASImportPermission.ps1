a45c15b2e15df15169380e745f8e6a623f4c419f
dnlrv <>
2025-05-15 13:47:50 -0400

    Added PASImportPermission class nad New-PASImportPermissions cmdlet

---
+++ Classes/PASImportPermission.ps1
@@ -0,0 +1,31 @@
+# class for holding Permission information for importing into another PAS tenant
+# a human readable format
+[NoRunspaceAffinity()]
+class PASImportPermission
+{
+    [System.String]$Principal   # the name of principal
+    [System.String]$PType       # the type of principal, User, Group, or Role
+    [System.String]$Rights      # the rights to assign
+    [System.String]$PrincipalId # the uuid of the principal
+
+	# empty constructor
+	PASImportPermission () {}
+
+	#primary constructor
+    PASImportPermission ([PSObject]$pra)
+    {
+        $this.Principal        = $pra.PrincipalName
+        $this.PType            = $pra.PrincipalType
+        $this.Rights           = $pra.PASPermission.GrantString
+        $this.PrincipalId      = $pra.PrincipalUuid
+    }# PASImportPermission ([PSObject]$pra)
+
+    updatePrincipalId()
+    {   
+        $uuid = $null
+        
+        Invoke-Expression -Command ('$uuid = Search-PASDirectory -{0} "{1}" | Select-Object -ExpandProperty ID' -f $this.PType, $this.Principal)
+
+        $this.PrincipalId = $uuid
+    }
+}# class PASImportPermission
---
+++ Functions/Private/New-PASImportPermission.ps1
@@ -0,0 +1,52 @@
+###########
+#region ### global:New-PASImportPermission # CMDLETDESCRIPTION : Builds a new PASImportPermission from existing PermissionRowAces :
+###########
+function global:New-PASImportPermission
+{
+    <#
+    .SYNOPSIS
+    Builds a new PASImportPermission from existing PermissionRowAces.
+
+    .DESCRIPTION
+    This cmdlet will build a PASImportPermission class object based on provided PermissionRowAces from existing PAS PCM objects.
+
+    The intent with this cmdlet is to help build a json body payload for recreating or setting permissions for principals in a 
+    PAS tenant.
+
+    This cmdlet will most likely be used with Build-PASGrantPermissionPayload to build the custom json body needed to update
+    PermissionRowAces in PAS.
+
+    .PARAMETER PermissionRowAces
+	The existing PermissionRowAces to import.
+
+    .INPUTS
+    None. You can't redirect or pipe input to this function.
+
+    .OUTPUTS
+    This function outputs an ArrayList of PASImportPermissions objects.
+
+    .EXAMPLE
+    C:\PS> $imports = New-PASImportPermission -PermissionRowAces $myrootAccount.PermissionRowAces
+    For each PermissionRowAces found in $myrootAccount, build one PASImportPermission object.
+    #>
+    [CmdletBinding(DefaultParameterSetName="All")]
+    param
+    (
+        [Parameter(Mandatory = $true, HelpMessage = "The offline data to reserialize.")]
+		[PSObject]$PermissionRowAces
+    )
+
+    # arraylist for the imports
+    $ImportsToReturn = New-Object System.Collections.ArrayList
+
+    foreach ($permissionrow in $PermissionRowAces)
+    {
+        $obj = New-Object PASImportPermission -ArgumentList $permissionrow
+
+        $ImportsToReturn.Add($obj) | Out-Null
+    }
+
+    return $ImportsToReturn
+}# function global:New-PASImportPermission
+#endregion
+###########
